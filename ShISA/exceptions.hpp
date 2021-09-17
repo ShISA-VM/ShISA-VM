ac74ec6b6c39b96339d27c45076f24241fbfbff7
cesarus777 <ulius.cesarus@gmail.com>
2021-09-17 11:35:03 +0300

    Add project-specific exceptions

---
+++ ShISA/exceptions.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "Inst.hpp"
+
+#include <array>
+#include <exception>
+#include <string>
+#include <string_view>
+
+
+
+namespace shisa {
+
+class Exception : public std::exception {
+private:
+  std::string msg;
+
+protected:
+  auto appendMsg(std::string_view str) -> std::string & {
+    return msg.append(str);
+  }
+
+public:
+  explicit Exception() : msg{"shisa::Exception"} {}
+  explicit Exception(std::string_view message) : msg{message.data()} {}
+
+  [[nodiscard]] auto what() const noexcept -> const char * override {
+    return msg.c_str();
+  }
+};
+
+
+
+template <typename T>
+concept IsInst = std::is_base_of<Inst, T>::value;
+
+template <IsInst InstClass>
+class UnexpectedInst : public Exception {
+private:
+  static constexpr size_t n = InstClass::opCodes.size();
+
+  InstClass             inst;
+  std::array<OpCode, n> expectedOps;
+
+public:
+  UnexpectedInst(InstClass *i, const std::array<OpCode, n> &opCodes)
+      : Exception{"UnexpectedInst"}, inst{*i}, expectedOps{opCodes} {}
+
+  [[nodiscard]] auto got() const noexcept -> InstClass { return inst; }
+
+  [[nodiscard]] auto expected() const noexcept
+      -> const std::array<OpCode, n> & {
+    return expectedOps;
+  }
+};
+
+
+
+template <typename T>
+class InvalidValue : public Exception {
+public:
+  InvalidValue(const char *var_name, T var_val) : Exception{var_name} {
+    appendMsg(" == ");
+    appendMsg(std::to_string(var_val));
+  }
+};
+
+
+
+class InvalidInst : public Exception {
+public:
+  InvalidInst(Inst inst) : Exception{"Invalid instruction: "} {
+    appendMsg("with opcode");
+    appendMsg(std::to_string(static_cast<unsigned>(inst.getOpCode())));
+  }
+};
+
+} // namespace shisa
+
