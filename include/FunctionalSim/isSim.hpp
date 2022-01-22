93b7b76fef1db5130ed9d015d77b484eb48092bc
cesarus777 <ulius.cesarus@gmail.com>
2022-01-22 16:45:24 +0300

    Add function for getting sim name

--- include/FunctionalSim/isSim.hpp
+++ include/FunctionalSim/isSim.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
-#include "FunctionalSim/Sim.hpp"
+#include "Sim.hpp"
+
+#include "PredecodedSim.hpp"
+#include "PredecodedSubroutinedSim.hpp"
+#include "SubroutinedSim.hpp"
+#include "SwitchedSim.hpp"
 
 #include <concepts>
 
@@ -8,6 +13,23 @@ namespace shisa::fsim {
 
 template <class S>
 concept isSim = std::derived_from<S, SimBase<typename S::Reg, typename S::Addr,
-                                              typename S::Cell, shisa::NREGS>>;
+                                             typename S::Cell, shisa::NREGS>>;
+
+template <isSim Sim>
+static auto getSimName() -> std::string {
+  if (shisa::fsim::isSwitchedSim<Sim>) {
+    return std::move(std::string{"SwitchedSim"});
+  }
+  if (shisa::fsim::isPredecodedSim<Sim>) {
+    return std::move(std::string{"PredecodedSim"});
+  }
+  if (shisa::fsim::isSubroutinedSim<Sim>) {
+    return std::move(std::string{"SubroutinedSim"});
+  }
+  if (shisa::fsim::isPredecodedSubroutinedSim<Sim>) {
+    return std::move(std::string{"PredecodedSubroutinedSim"});
+  }
+  return std::move(std::string{"UnknownSim"});
+}
 
 } // namespace shisa::fsim
