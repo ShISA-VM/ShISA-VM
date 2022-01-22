ed0300a4b8f5d6a1cc86023026824ccba12e836b
cesarus777 <ulius.cesarus@gmail.com>
2022-01-22 16:53:39 +0300

    Add tests for predecoded, subroutined and predecoded subroutined sims

---
+++ test/FunctionalSim/SubroutinedSim.cpp
@@ -0,0 +1,30 @@
+#include "SimTester.hpp"
+
+#include <FunctionalSim/SubroutinedSim.hpp>
+#include <exceptions.hpp>
+
+using Reg  = uint16_t;
+using Addr = uint16_t;
+using Cell = uint8_t;
+
+using SimTester = shisa::test::SimTester<Reg, Addr, Cell, shisa::NREGS,
+                                         shisa::fsim::SubroutinedSim>;
+
+
+
+int main() {
+  try {
+    SimTester::runTests();
+  } catch (const shisa::test::Exception &e) {
+    std::cerr << __FILE__ ": test fail: " << e.what() << std::endl;
+    exit(EXIT_FAILURE);
+  } catch (const std::exception &e) {
+    std::cerr << __FILE__ ": test fail: exception caught in main: " << e.what()
+              << std::endl;
+    exit(EXIT_FAILURE);
+  } catch (...) {
+    std::cerr << __FILE__ ": test fail: unknown exception caught in main"
+              << std::endl;
+    exit(EXIT_FAILURE);
+  }
+}
