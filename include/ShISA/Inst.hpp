4937605b0a8825ab087b3e9b4baf5f8e67fe842d
cesarus777 <ulius.cesarus@gmail.com>
2022-01-22 16:37:41 +0300

    Add XOR opcode and refactor instruction decoding

--- include/ShISA/Inst.hpp
+++ include/ShISA/Inst.hpp
@@ -10,14 +10,15 @@
 namespace shisa {
 
 enum class OpCode {
-  ADD  = 0x01,
-  SUB  = 0x02,
-  MUL  = 0x03,
-  DIV  = 0x04,
-  AND  = 0x05,
-  OR   = 0x06,
-  CMP  = 0x07,
-  NOT  = 0x08,
+  ADD  = 0x00,
+  SUB  = 0x01,
+  MUL  = 0x02,
+  DIV  = 0x03,
+  AND  = 0x04,
+  OR   = 0x05,
+  XOR  = 0x06,
+  NOT  = 0x07,
+  CMP  = 0x08,
   JTR  = 0x09,
   LD   = 0x0a,
   ST   = 0x0b,
@@ -37,6 +38,8 @@ class InstBase {
   Inst inst;
 
 public:
+  InstBase() : inst{} {}
+
   InstBase(Inst i) : inst{i} {}
 
   using RawInst = Inst;
@@ -50,6 +53,9 @@ public:
 template <>
 class InstBase<uint16_t> {
   uint16_t inst;
+
+public:
+  struct DecodedInst;
 
   static constexpr unsigned mask = 0x0f;
 
@@ -66,24 +72,15 @@ class InstBase<uint16_t> {
   static constexpr std::size_t dstShift    = dstPos * posSize;
   static constexpr std::size_t opCodeShift = opCodePos * posSize;
 
-public:
   using RawInst = uint16_t;
+
+  InstBase() : inst{} {}
 
   InstBase(RawInst i) : inst{i} {}
 
   operator RawInst() const { return inst; }
 
-  [[nodiscard]] inline auto getOpCode() const -> OpCode {
-    return static_cast<OpCode>((inst >> opCodeShift) & mask);
-  }
-
-  [[nodiscard]] auto decode() const {
-    int    srcR = (inst >> srcRShift) & mask;
-    int    srcL = (inst >> srcLShift) & mask;
-    int    dst  = (inst >> dstShift) & mask;
-    OpCode op   = getOpCode();
-    return std::make_tuple(op, dst, srcL, srcR);
-  }
+  [[nodiscard]] auto decode() const -> DecodedInst { return DecodedInst{inst}; }
 
   constexpr static auto encode(OpCode opCode, int dst, int srcL, int srcR)
       -> RawInst {
@@ -96,9 +93,27 @@ public:
   }
 
   constexpr static std::array opCodes = {
-      OpCode::ADD, OpCode::SUB,  OpCode::MUL, OpCode::DIV,  OpCode::AND,
-      OpCode::OR,  OpCode::CMP,  OpCode::NOT, OpCode::JTR,  OpCode::LD,
-      OpCode::ST,  OpCode::PUSH, OpCode::POP, OpCode::CALL, OpCode::RET};
+      OpCode::ADD,  OpCode::SUB, OpCode::MUL,  OpCode::DIV,
+      OpCode::AND,  OpCode::OR,  OpCode::XOR,  OpCode::NOT,
+      OpCode::CMP,  OpCode::JTR, OpCode::LD,   OpCode::ST,
+      OpCode::PUSH, OpCode::POP, OpCode::CALL, OpCode::RET};
+
+  struct DecodedInst {
+    OpCode opCode;
+    int    dst;
+    int    srcL;
+    int    srcR;
+
+    DecodedInst(RawInst i) {
+      opCode = static_cast<OpCode>((i >> opCodeShift) & mask);
+      dst    = (i >> dstShift) & mask;
+      srcL   = (i >> srcLShift) & mask;
+      srcR   = (i >> srcRShift) & mask;
+    }
+
+    DecodedInst(OpCode o, int d, int l, int r)
+        : opCode{o}, dst{d}, srcL{l}, srcR{r} {}
+  };
 };
 
 using Reg  = uint16_t;
